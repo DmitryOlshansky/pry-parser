2e320f6ff40d3769006b115c2136ab7fdb29c0cb
Dmitry Olshansky <dmitry.olsh@gmail.com>
2013-10-22 00:31:37 +0400

    outline more of the parser/ast; unittests for some internals

--- dpick/ast.d
+++ dpick/ast.d
@@ -3,7 +3,12 @@ module dpick.ast;
 //
 class DataExpr
 {
+@safe pure:
     DataPiece[] items;
+    this(DataPiece[] pieces)
+    {
+        items = pieces;
+    }
 }
 
 class DataAlt : DataExpr
@@ -25,6 +30,56 @@ class DataSeq : DataExpr
 }
 
 class DataPiece
+{
+@safe pure:
+    DataAtom atom;
+    int low, high;
+    this(DataAtom a, int l, int h)
+    {
+        atom = a;
+        low = l;
+        high = h;
+    }
+}
+
+class DataAtom
+{
+@safe pure:
+    AliasExpr aliases;
+    this(AliasExpr a)
+    {
+        aliases = a;
+    }
+}
+
+class EntityAtom : DataAtom
+{
+@safe pure:
+    EntityExpr entity;
+    this(EntityExpr e, AliasExpr a)
+    {
+        super(a);
+        entity = e;
+    }
+}
+
+class ExprAtom : DataAtom
+{
+@safe pure:
+    DataExpr expr;
+    this(DataExpr e, AliasExpr a)
+    {
+        super(a);
+        expr = e;        
+    }
+}
+
+class AliasExpr
+{
+
+}
+
+class EntityExpr
 {
 
 }
--- dpick/parser.d
+++ dpick/parser.d
@@ -2,7 +2,7 @@ module dpick.parser;
 
 import dpick.ast;
 
-import std.algorithm, std.range, std.exception, std.uni;
+import std.algorithm, std.range, std.exception, std.ascii;
 
 class ParseException : Exception
 {
@@ -17,6 +17,7 @@ class ParseException : Exception
 private struct Parser
 {
 @safe pure:
+    enum INF = -1;
     string input;
     int line;
     DataExpr[string] decls;
@@ -25,6 +26,10 @@ private struct Parser
     {
         input = inp;
         line = 1;
+    }
+
+    auto parse()
+    {
         skipWs();
         while(!input.empty)
         {            
@@ -33,6 +38,7 @@ private struct Parser
         }
         if("root" !in decls)
             error("no 'root' entity defined");
+        return decls;
     }
 
     //Declaration : id '=' DataExpr ';'
@@ -45,11 +51,6 @@ private struct Parser
         if(id in decls)
             error("double definition of "~id);
         decls[id] = expr;
-    }
-
-    DataPiece parseDataPiece()
-    {
-        return new DataPiece();
     }
 
     //DataExpr : DataSequence
@@ -79,8 +80,66 @@ private struct Parser
             }
             while(match(','));
         }
-        //falthrough - 1 item is also sequence
+        //falthrough - 1 item is also a sequence
         return new DataSeq(pieces);
+    }
+
+    //DataPiece : DataAtom
+    //          : DataAtom '*'
+    //          : DataAtom '?'
+    //          : DataAtom '{' Expr (',' Expr )? '}'
+    DataPiece parseDataPiece()
+    {
+        DataAtom a = parseDataAtom();
+        int low = 1, high = 1;
+        if(match("?"))
+        {
+            low = 0;
+        }
+        else if(match("*")) 
+        {
+            low = 0;
+            high = INF;
+        }
+        else if(match("+"))
+        {
+            low = 1;
+            high = INF;
+        }
+        else if(match("{"))
+        {
+
+        }
+        return new DataPiece(a, low, high);
+    }
+
+    //DataAtom : EntityExpr AliasExpr 
+    //         : '(' DataExpr ')' AliasExpr
+    DataAtom parseDataAtom()
+    {
+        if(match('('))
+        {
+            DataExpr expr = parseDataExpr();
+            auto ret =  new ExprAtom(expr, parseAliasExpr());
+            check(')');
+            return ret;
+        }        
+        EntityExpr expr = parseEntityExpr();        
+        return new EntityAtom(expr, parseAliasExpr());
+    }
+
+    //AliasExpr : '!'? Name? (':' AliasAtom*)?
+    AliasExpr parseAliasExpr()
+    {
+        return new AliasExpr();
+    }
+
+    //EntityExpr : Name 
+    //           : BytePattern
+    //           : StringPattern
+    EntityExpr parseEntityExpr()
+    {
+        return new EntityExpr();
     }
 
     //Name : [a-zA-Z_][a-zA-Z_0-9]*
@@ -91,11 +150,83 @@ private struct Parser
         if(!isAlpha(input.front))
             error("expected alphabetic character");
         input.popFront();        
-        input = input.find!(x => !isAlpha(x) && !isNumber(x));
+        input = input.find!(x => !isAlpha(x) && !isDigit(x));
         return save[0..$-input.length];
     }
 
-    bool match(char[] piece...)
+    //Number : [0-9]+
+    //       : '0' 'x' [0-9A-Fa-f]+
+    int parseNum()
+    {
+        skipWs();
+        if(!isDigit(input.front))
+            error("expected digit character");        
+        int val = input.front - '0';
+        input.popFront();
+        if(val == 0 && input.front == 'x')
+        {
+            input.popFront();
+            return parseHex();
+        }
+        while(!input.empty)
+        {
+            auto ch = input.front;
+            if(!isDigit(ch))
+                break;
+            val = 10 * val + ch - '0';
+            if(val > 255)
+                error("values may not exceed the range [0, 255]");
+            input.popFront();
+        }
+        return val;
+    }
+
+    unittest
+    {
+        assert(collectException(Parser("0x").parseNum()));
+        assert(Parser("0x81").parseNum() == 0x81);
+        assert(Parser("12").parseNum() == 12);
+        assert(Parser(" 01").parseNum() == 1);
+    }
+
+    // [0-9A-Fa-f]+
+    int parseHex()
+    {
+        int val = 0;
+        if(input.empty)
+            error("unexpected end of input");
+        if(!isHexDigit(input.front))
+            error("expected hex-digit character");
+        do
+        {
+            auto ch = input.front;
+            int d;
+            if(ch >= '0' && ch <= '9')
+                d = ch - '0';
+            else if(ch >= 'A' && ch <= 'F') 
+                d = ch - 'A' + 10;
+            else if(ch >= 'a' && ch <= 'f') 
+                d = ch - 'a' + 10;
+            else
+                break;
+            val = 16 * val + d;
+            if(val > 255)
+                error("values may not exceed the range [0, 255]");
+            input.popFront();
+        }while(!input.empty);
+        return val;        
+    }
+
+    unittest
+    {
+        assert(Parser("12").parseHex() ==  0x12);
+        assert(Parser("FF").parseHex() ==  255);
+        assert(Parser("aB").parseHex() ==  0xaB);
+        assert(collectException(Parser("Ga").parseHex()));
+        assert(Parser("Ag").parseHex() == 10);
+    }
+
+    bool match(const(char)[] piece...)
     {
         skipWs();
         if(input.skipOver(piece))
@@ -105,7 +236,7 @@ private struct Parser
         return false;
     }
 
-    void check(char[] piece...)
+    void check(const(char)[] piece...)
     {
         if(!match(piece))
             error("expected: " ~ piece.idup);
@@ -116,10 +247,18 @@ private struct Parser
         for(;;)
         {
             auto f = input.front;
+            if(!isWhite(f))
+                break;
             if(f == '\n')
                 line++;
             input.popFront();
         }
+    }
+
+    unittest
+    {
+        assert(Parser(" \nabc").match("abc"));
+        assert(Parser("abc").match('a'));
     }
 
     void error(string msg)
