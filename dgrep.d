7dd838b87717e736090cb22bc77ca329c85da3b4
Dmitry Olshansky <dmitry.olsh@gmail.com>
2013-12-09 17:27:40 +0400

    add example (still bogus) of regex on I/O backed buffers

---
+++ dgrep.d
@@ -0,0 +1,23 @@
+import std.stdio, std.datetime;
+import dpick.buffer;
+import regex;
+
+
+void main(string[] args)
+{
+    if(args.length < 3) {
+        writefln("Usage: %s <pattern> [file]", args[0]);
+        return;
+    }
+    auto buf = buffer(fileInput(args[2]), 4096);
+    int count;
+    StopWatch sw;
+    sw.start();
+    foreach(m; buf.matchAll(args[1]))
+    {
+        count++;
+        writeln(cast(string)m[0]);
+    }
+    sw.stop();
+    writefln("%d matches in %s ms", count, sw.peek().msecs);
+}
